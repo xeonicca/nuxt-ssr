1198b935fa38fd80d4f309cd2470a4f6d86f09a7
Steve <soundedrain@gmail.com>
2020-03-16 01:27:42 +0800

    initial commit

---
+++ pages/posts/_id.vue
@@ -0,0 +1,26 @@
+<template>
+  <article>
+    <h2>{{ title }}</h2>
+    <p>{{ body }}</p>
+    <nuxt-link to="/posts">Back</nuxt-link>
+  </article>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  asyncData ({ params }) {
+    return axios.get(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+      .then(res => {
+        return res.data
+      })
+  },
+
+  head() {
+    return {
+      title: this.title
+    }
+  }
+}
+</script>
---
+++ pages/posts/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <article>
+    <h2>{{ title }}</h2>
+    <ul v-for="post in posts">
+      <li><nuxt-link :to="{ name: 'posts-id', params: { id: post.id }}">{{ post.title }}</nuxt-link></li>
+    </ul>
+  </article>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  asyncData ({ params }) {
+    return axios.get(`https://jsonplaceholder.typicode.com/posts`)
+      .then(res => {
+        return {
+          posts: res.data
+        }
+      })
+  }
+}
+</script>
